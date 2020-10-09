59257b2891f19378b67c295cf3641f9df86d1160
fh_bots <69167693+fh-bots@users.noreply.github.com>
2020-10-10 02:17:41 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -61,14 +61,10 @@ START_OTHER_USERS_TEXT = get_config(
     (
         "Hi. ☺️\n"
         "Thank you for using me 😬\n\n"
-        "This is an Open Source Project available on "
-        "https://github.com/SpEcHIDe/NoPMsBot\n\n\n"
-        "If you are the owner of this bot, "
-        "and are seeing this message 🤦‍♂️, "
-        "means that you have not set up "
-        "the ENVironment variables properly "
-        "for the bot to function.\n\n\n"
-        "ℹ️ Subscribe @SpEcHlDe if you 😍 using this bot❗️❣️"
+        "This is simple bot,That let's you contact the admin "
+        
+        "\n\n\n"
+        "ℹ️ Subscribe @Fh_Bots| @FILIMHOUSE"
     )
 )
 # check online status of your bot
